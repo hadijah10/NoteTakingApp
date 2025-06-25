1afe01138c8898d4db3517665f35ad31af76a0a2
Your Name <mohamaduhadijatu@gmail.com>
2025-06-25 12:09:42 -0400

    fix:add styling for card responsiveness

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { INotes } from '../../../../public/interfaces/datainterface';
+import { ApiserviceService } from '../../api/apiservice.service';
 
 @Component({
   selector: 'app-homepage',
@@ -8,9 +10,27 @@ import { Component } from '@angular/core';
 })
 export class HomepageComponent {
 searchTerm:string = ''
+notelist!:INotes[]
+isLoading = true;
+errorMessage = ''
 
+constructor(private apiservice: ApiserviceService){
+  this.apiservice.getTodos().subscribe({
+    next:(data) => {
+      this.isLoading = false
+      this.notelist = data
+    },
+    error:(error => {
+      this.isLoading = false
+      this.errorMessage = error
+    })
+  })
+}
 handleSearch(event:Event){
   const target = event.target as HTMLInputElement
  console.log(target.value)
 }
+
+
+
 }
