56c53c2ef7ed35f61af5b0244be2f50707812d3e
Your Name <mohamaduhadijatu@gmail.com>
2025-06-25 20:06:01 -0400

    feat:add edit page form

---
+++ src/app/components/editform/editform.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editform',
+  imports: [],
+  templateUrl: './editform.component.html',
+  styleUrl: './editform.component.scss'
+})
+export class EditformComponent {
+
+}
